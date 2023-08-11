80efe3c7f82108f6809d17c367aba2038b9e0f54
yisak <yisakbruck23@gmail.com>
2023-08-11 16:57:08 -0400

    complete lab

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,81 @@
-class Author:
-    pass
-
-
 class Book:
-    pass
+    _all = []
+
+    def __init__(self, title):
+        self._title = title
+        Book._all.append(self)
+
+    @property
+    def title(self):
+        return self._title
+
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.book == self]
+
+    def authors(self):
+        return list({contract.author for contract in self.contracts()})
+
+
+class Author:
+    _all = []
+
+    def __init__(self, name):
+        self._name = name
+        Author._all.append(self)
+
+    @property
+    def name(self):
+        return self._name
+
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.author == self]
+
+    def books(self):
+        return [contract.book for contract in self.contracts()]
+
+    def sign_contract(self, book, date, royalties):
+        return Contract(self, book, date, royalties)
+
+    def total_royalties(self):
+        return sum(contract.royalties for contract in self.contracts())
 
 
 class Contract:
-    pass
+    all = []
+
+    def __init__(self, author, book, date, royalties):
+        if not isinstance(author, Author):
+            raise Exception("Author must be an instance of the Author class.")
+        if not isinstance(book, Book):
+            raise Exception("Book must be an instance of the Book class.")
+        if not isinstance(date, str):
+            raise Exception("Date must be of type str.")
+        if not isinstance(royalties, int):
+            raise Exception("Royalties must be of type int.")
+        
+        self._author = author
+        self._book = book
+        self._date = date
+        self._royalties = royalties
+
+        Contract.all.append(self)
+
+    @property
+    def author(self):
+        return self._author
+
+    @property
+    def book(self):
+        return self._book
+
+    @property
+    def date(self):
+        return self._date
+
+    @property
+    def royalties(self):
+        return self._royalties
+
+    @classmethod
+    def contracts_by_date(cls):
+        return sorted(cls.all, key=lambda contract: contract.date)
